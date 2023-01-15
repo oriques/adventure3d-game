92202209c3759f291d499de7110eedeebd7eb9fa
Oriques de Souza <104857614+oriques@users.noreply.github.com>
2023-01-15 10:22:37 -0300

    Audio Config in Game Working

--- Assets/Scripts/Audio/AudioChangeVolume.cs
+++ Assets/Scripts/Audio/AudioChangeVolume.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Audio;
 
 public class AudioChangeVolume : MonoBehaviour
 {
     public AudioMixer group;
     public string floatParam = "";
+    public Slider slider;
 
     public void ChangeValue(float f)
     {
-        group.SetFloat(floatParam, f);
+        group.SetFloat(floatParam, slider.value);
     }
 
 
